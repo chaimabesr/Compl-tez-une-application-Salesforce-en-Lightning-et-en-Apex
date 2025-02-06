ea57799cc468c88f7ac6748dccc67cf03d836bee
besrour walid <68786804+walidbesrour@users.noreply.github.com>
2025-02-06 15:45:48 +0100

    Projet 3 Chaima Gharbi

--- force-app/main/default/lwc/accountCasesSearcher/accountCasesSearcher.js
+++ force-app/main/default/lwc/accountCasesSearcher/accountCasesSearcher.js
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 import { LightningElement, track, api } from 'lwc';
 import findCasesBySubject from '@salesforce/apex/AccountCasesController.findCasesBySubject';
+=======
+import { LightningElement, track, api, wire } from 'lwc';
+import findCasesBySubject from '@salesforce/apex/AccountCasesController.getCases';
+import Subject from '@salesforce/schema/Case.Subject';
+>>>>>>> Projet 3 Chaima Gharbi
 
 const COLUMNS = [
     { label: 'Sujet', fieldName: 'Subject', type: 'text' },
@@ -15,6 +21,7 @@ export default class AccountCaseSearchComponent extends LightningElement {
     columns = COLUMNS;
 
     updateSearchTerm(event) {
+<<<<<<< HEAD
         this.searchTerm = event.target.value;
     }
 
@@ -25,6 +32,21 @@ export default class AccountCaseSearchComponent extends LightningElement {
                 this.error = undefined;
             })
             .catch(error => {
+=======
+        this.searchTerm= event.target.value;
+    }
+
+    handleSearch() {
+        console.log('bouton appuyé');
+        findCasesBySubject({ accountId: this.recordId, subject: this.searchTerm })
+            .then(result => {
+                this.cases = result;
+                this.error = undefined;
+                console.log(this.cases);
+            })
+            .catch(error => {
+                console.log(error);
+>>>>>>> Projet 3 Chaima Gharbi
                 this.error = 'Une erreur est survenue lors de la recherche des cases.';
             });
     }
--- force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
+++ force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
@@ -1,16 +1,29 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getOpportunities from '@salesforce/apex/AccountOpportunitiesController.getOpportunities';
+<<<<<<< HEAD
+=======
+import { refreshApex } from '@salesforce/apex';
+
+>>>>>>> Projet 3 Chaima Gharbi
 
 export default class AccountOpportunitiesViewer extends LightningElement {
     @api recordId;
     @track opportunities;
     @track error = {};
+<<<<<<< HEAD
+=======
+    @track showError = false; // Contrôle si le message d'erreur doit être affiché
+    wiredResult; // Pour stocker les données renvoyées par l'@wire
+
+    // Configuration des colonnes pour lightning-datatable
+>>>>>>> Projet 3 Chaima Gharbi
     columns = [
         { label: 'Nom Opportunité', fieldName: 'Name', type: 'text' },
         { label: 'Montant', fieldName: 'Amount', type: 'currency' },
         { label: 'Date de Clôture', fieldName: 'CloseDate', type: 'date' },
         { label: 'Phase', fieldName: 'StageName', type: 'text' }
     ];
+<<<<<<< HEAD
 
     @wire(getOpportunities, { recordId: '$accountId' }) //error
     wiredOpportunities({ error, data }) {
@@ -21,6 +34,40 @@ export default class AccountOpportunitiesViewer extends LightningElement {
             this.opportunities = undefined;
         }
     }
+=======
+ // Chargement des opportunités via une méthode Apex
+    @wire(getOpportunities, { accountId: '$recordId' }) //error
+    wiredOpportunities(result) {
+        this.wiredResult = result; // Stocker le résultat pour le rafraîchissement
+        const { error, data } = result;
+
+        if (data) {
+            this.opportunities = data; // Affectation des opportunités
+            this.showError = false; // Réinitialiser l'affichage de l'erreur
+        } else if (error) {
+            this.error = error; // Stocker l'erreur
+            this.opportunities = undefined;
+            this.showError = true; // Activer l'affichage de l'erreur
+
+        }
+        
+  // Gestion du bouton Rafraîchir
+
+    }
+    handleRafraichir() {
+        
+        if  (this.wiredResult) { // Vérifier si l'@wire est déjà défini
+            refreshApex(this.wiredResult)  // Rafraîchir les données liées à l'@wire
+                .then(() => {
+                    console.log('Données rafraîchies avec succès');
+                })
+                .catch((error) => {
+                    console.error('Erreur lors du rafraîchissement des données : ', error);
+                });
+        }
+    }
+    
+>>>>>>> Projet 3 Chaima Gharbi
 
 
 }
